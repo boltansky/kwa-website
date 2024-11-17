8bccd772bbdd7abcbfed6df6c1504dd1b16c81c1
coolatamaster <kwan@magental-2.local>
2024-11-17 16:43:29 +0900

    WCUBEC added

---
+++ WCUBEC/JHL_FEST-not working/gita_haengsa_files/gita_haengsa.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.1 local-build-20100526
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('gita_haengsa_files/gita_haengsaMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ WCUBEC/JHL_FEST-not working/yeonlagcheo_files/yeonlagcheo.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.1 local-build-20100526
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('yeonlagcheo_files/yeonlagcheoMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
